179345f13ab9bef78b7d76ad98a63b2f194117a8
Raffaele P. Guidi <raffaele.p.guidi@gmail.com>
2015-03-11 11:51:42 +0100

    removed compiled files from git

--- public/javascripts/compiled/item-detail.js
+++
@@ -1,3 +0,0 @@
-riot.tag('item-detail', '<div if="{ opts.item }"> <h3>Dettagli</h3> <div><b>ID:</b> { opts.item.id }</div> <div><b>Nome:</b> { opts.item.title }</div> </div>', function(opts) {
-
-});
